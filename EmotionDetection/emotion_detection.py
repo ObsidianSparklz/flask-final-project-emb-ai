ecf20328cef28dc9b44078ed486d043f80eff3e1
obsidiansparklz <obsidiansparklz@gmail.com>
2025-07-17 17:17:07 -0400

    Task 7 update

--- EmotionDetection/emotion_detection.py
+++ EmotionDetection/emotion_detection.py
@@ -2,12 +2,20 @@ import requests
 import json
 
 def emotion_detector(text_to_analyse):
+    if not text_to_analyse or not isinstance(text_to_analyse, str) or text_to_analyse.strip() == "":
+        return None
     url = 'https://sn-watson-emotion.labs.skills.network/v1/watson.runtime.nlp.v1/NlpService/EmotionPredict'
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
-    response = requests.post(url, json = myobj, headers=header)  
-    formatted_response = json.loads(response.text)
-    emotion_scores = formatted_response['emotionPredictions'] [0] ['emotion']
-    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
-    emotion_scores['dominant_emotion'] = dominant_emotion
-    return emotion_scores
+    try:
+        response = requests.post(url, json = myobj, headers=header)
+        if response.status_code == 200:
+            formatted_response = json.loads(response.text)
+            emotion_scores = formatted_response['emotionPredictions'] [0] ['emotion']
+            dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+            emotion_scores['dominant_emotion'] = dominant_emotion
+            return emotion_scores
+        elif response.status_code == 400:
+            return None
+    except (requests.exceptions.RequestException, KeyError, IndexError):
+        return None
