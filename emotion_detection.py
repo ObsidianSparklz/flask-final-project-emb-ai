42db6692ad71725671d6f3d834ed13805ca75ce6
obsidiansparklz <obsidiansparklz@gmail.com>
2025-07-17 14:31:37 -0400

    change 3

--- emotion_detection.py
+++ emotion_detection.py
@@ -6,4 +6,8 @@ def emotion_detector(text_to_analyse):
     myobj = { "raw_document": { "text": text_to_analyse } }
     header = {"grpc-metadata-mm-model-id": "emotion_aggregated-workflow_lang_en_stock"}
     response = requests.post(url, json = myobj, headers=header)  
-    return response.text
+    formatted_response = json.loads(response.text)
+    emotion_scores = formatted_response['emotionPredictions'] [0] ['emotion']
+    dominant_emotion = max(emotion_scores, key=emotion_scores.get)
+    emotion_scores['dominant_emotion'] = dominant_emotion
+    return emotion_scores
